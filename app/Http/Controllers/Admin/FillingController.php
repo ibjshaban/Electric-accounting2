34eb6437f6873729529f75766d8b0ed9a83f8330
SamerAyash <sam.ayash200@gmail.com>
2022-01-05 19:17:16 +0300

    filling table

--- app/Http/Controllers/Admin/FillingController.php
+++ app/Http/Controllers/Admin/FillingController.php
@@ -97,7 +97,7 @@ class FillingController extends Controller
                     return redirect()->back()->withErrors('لم تتم العملية حدث خطأ ما')->withInput();
 
                 }
-                $redirect = isset($request["add_back"])?"filling/create":"supplier/".$supplier_id;
+                $redirect = isset($request["add_back"]) ?"filling/create":"supplier/".$supplier_id;
                 return redirectWithSuccess(aurl($redirect), trans('admin.added')); }
 
             /**
@@ -110,7 +110,7 @@ class FillingController extends Controller
             {
         		$filling =  Filling::find($id);
         		return is_null($filling) || empty($filling)?
-        		backWithError(trans("admin.undefinedRecord"),aurl("filling")) :
+        		backWithError(trans("admin.undefinedRecord"),url()->previous()) :
         		view('admin.filling.show',[
 				    'title'=>trans('admin.show'),
 					'filling'=>$filling
@@ -126,11 +126,13 @@ class FillingController extends Controller
             public function edit($id)
             {
         		$filling =  Filling::find($id);
+                $stocks= Stock::all()->sortBy('name');
         		return is_null($filling) || empty($filling)?
-        		backWithError(trans("admin.undefinedRecord"),aurl("filling")) :
+        		backWithError(trans("admin.undefinedRecord"),url()->previous()) :
         		view('admin.filling.edit',[
 				  'title'=>trans('admin.edit'),
-				  'filling'=>$filling
+				  'filling'=>$filling,
+                    'stocks'=> $stocks
         		]);
             }
 
@@ -156,12 +158,45 @@ class FillingController extends Controller
               // Check Record Exists
               $filling =  Filling::find($id);
               if(is_null($filling) || empty($filling)){
-              	return backWithError(trans("admin.undefinedRecord"),aurl("filling"));
+              	return backWithError(trans("admin.undefinedRecord"),url()->previous());
               }
               $data = $this->updateFillableColumns();
-              Filling::where('id',$id)->update($data);
-              $redirect = isset($request["save_back"])?"/".$id."/edit":"";
-              return redirectWithSuccess(aurl('filling'.$redirect), trans('admin.updated'));
+             // Filling::where('id',$id)->update($data);
+
+                DB::beginTransaction();
+                try {
+                    $filling = new Filling();
+                    $filling->name= $data['name'];
+                    $filling->quantity= $data['quantity'];
+                    $filling->price= $data['price'];
+                    $filling->filling_date= $data['filling_date'];
+                    $filling->note= $data['filling_note'];
+                    $filling->save();
+
+                    $paid_price=[];
+                    foreach ($filling->fule() as $fule) { array_push($fule->paid_amount,$paid_price);$fule->delete();}
+                    for ($i=0; $i < count($data['amount']??[]); $i++){
+                        RevenueFule::create([
+                            'quantity'=> InsertLargeNumber($data['amount'][$i]),
+                            'price'=> InsertLargeNumber($data['price']),
+                            'paid_amount'=> 0,
+                            'stock_id'=> $data['stock'][$i],
+                            'revenue_id'=> $data['revenue'][$i],
+                            'city_id'=> Stock::whereId($data['stock'][$i])->first()->city_id,
+                            'note'=> $data['note'][$i],
+                            'filling_id'=> $filling->id,
+                        ]);
+                    }
+                    DB::commit();
+                }
+                catch (\Exception $e){
+                    DB::rollBack();
+                    return redirect()->back()->withErrors('لم تتم العملية حدث خطأ ما')->withInput();
+
+                }
+
+                $redirect = isset($request["save_back"])?"filling/".$id."/edit":"supplier/".$supplier_id;
+              return redirectWithSuccess(aurl($redirect), trans('admin.updated'));
             }
 
             /**
@@ -173,12 +208,13 @@ class FillingController extends Controller
 	public function destroy($id){
 		$filling = Filling::find($id);
 		if(is_null($filling) || empty($filling)){
-			return backWithSuccess(trans('admin.undefinedRecord'),aurl("filling"));
+			return backWithSuccess(trans('admin.undefinedRecord'),url()->previous());
 		}
 
 		it()->delete('filling',$id);
+		foreach ($filling->fule() as $fule) { $fule->delete();}
 		$filling->delete();
-		return redirectWithSuccess(aurl("filling"),trans('admin.deleted'));
+		return redirectWithSuccess(url()->previous(),trans('admin.deleted'));
 	}
 
 
@@ -188,22 +224,24 @@ class FillingController extends Controller
 			foreach($data as $id){
 				$filling = Filling::find($id);
 				if(is_null($filling) || empty($filling)){
-					return backWithError(trans('admin.undefinedRecord'),aurl("filling"));
+					return backWithError(trans('admin.undefinedRecord'),url()->previous());
 				}
 
 				it()->delete('filling',$id);
+                foreach ($filling->fule() as $fule) { $fule->delete();}
 				$filling->delete();
 			}
-			return redirectWithSuccess(aurl("filling"),trans('admin.deleted'));
+			return redirectWithSuccess(url()->previous(),trans('admin.deleted'));
 		}else {
 			$filling = Filling::find($data);
 			if(is_null($filling) || empty($filling)){
-				return backWithError(trans('admin.undefinedRecord'),aurl("filling"));
+				return backWithError(trans('admin.undefinedRecord'),url()->previous());
 			}
 
 			it()->delete('filling',$data);
+            foreach ($filling->fule() as $fule) { $fule->delete();}
 			$filling->delete();
-			return redirectWithSuccess(aurl("filling"),trans('admin.deleted'));
+			return redirectWithSuccess(url()->previous(),trans('admin.deleted'));
 		}
 	}
 
